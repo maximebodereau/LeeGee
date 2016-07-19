977eb9c70fbc2ffdf0229f4ef94d644922c384ef
maxime bodereau <maximebodereau@gmail.com>
2016-07-19 14:36:05 -0400

    BABYLON JS BASE

---
+++ LeeGee/client/main.js
@@ -0,0 +1,44 @@
+import BABYLON from 'babylonjs';
+
+
+// Global variables
+var canvas, engine, scene, camera, score = 0;
+var TOAD_MODEL;
+
+// An array to store each ending of the lane
+var ENDINGS = [];
+
+/**
+* Load the scene when the canvas is fully loaded
+*/
+document.addEventListener("DOMContentLoaded", function () {
+    if (BABYLON.Engine.isSupported()) {
+        initScene();
+    }
+}, false);
+
+/**
+ * Creates a new BABYLON Engine and initialize the scene
+ */
+function initScene() {
+    // Get canvas
+    canvas = document.getElementById("renderCanvas");
+
+    // Create babylon engine
+    engine = new BABYLON.Engine(canvas, true);
+
+    // Create scene
+    scene = new BABYLON.Scene(engine);
+
+    // Create the camera
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0,4,-10), scene);
+    camera.setTarget(new BABYLON.Vector3(0,0,10));
+    camera.attachControl(canvas);
+
+    // Create light
+    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0,5,-5), scene);
+
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+}
