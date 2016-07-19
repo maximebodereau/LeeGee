ef26a3ddb1c77b16a80b2ac47d06212b3f3a1ad9
maxime bodereau <maximebodereau@gmail.com>
2016-07-19 17:20:59 -0400

    Base Background + Box

--- LeeGee/client/main.js
+++ LeeGee/client/main.js
@@ -38,28 +38,37 @@ function initScene() {
     // Skybox
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox/skybox", scene);
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    skybox.material = skyboxMaterial;
+        skyboxMaterial.backFaceCulling = false;
+        skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("skybox", scene);
+        skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        skybox.material = skyboxMaterial;
 
     // Create light
-    var light = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 10, 0), scene);
         light.diffuse = new BABYLON.Color3(1, 1, 1);
-        light.specular = new BABYLON.Color3(1, 1, 1);
+        light.specular = new BABYLON.Color3(5, 5, 5);
         light.groundColor = new BABYLON.Color3(0, 0, 0);
 
 
-        var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
-      var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
-      extraGroundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
-      extraGroundMaterial.diffuseTexture.uScale = 60;
-      extraGroundMaterial.diffuseTexture.vScale = 60;
-      extraGround.position.y = -2.05;
-      extraGround.material = extraGroundMaterial;
+    var extraGround = BABYLON.Mesh.CreateGround("extraGround", 1000, 1000, 1, scene, false);
+    var extraGroundMaterial = new BABYLON.StandardMaterial("extraGround", scene);
+        extraGroundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+        extraGroundMaterial.diffuseTexture.uScale = 60;
+        extraGroundMaterial.diffuseTexture.vScale = 60;
+        extraGround.position.y = -2.05;
+        extraGround.material = extraGroundMaterial;
 
+
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap("ground", "heightMap.png", 1000, 1000, 100, 0, 10, scene, false);
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+        groundMaterial.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
+        groundMaterial.diffuseTexture.uScale = 10;
+        groundMaterial.diffuseTexture.vScale = 10;
+        groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        ground.position.y = -8.0;
+        ground.material = groundMaterial;
 
 
 
